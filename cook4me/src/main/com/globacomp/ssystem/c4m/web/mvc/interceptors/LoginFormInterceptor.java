26e2bb1af064f10c6621d870d44beee3ca13752a
Mihir Gohel <mgohel@apttus.com>
2016-04-29 22:56:33 +0530

    Login Interceptor Added

---
+++ cook4me/src/main/com/globacomp/ssystem/c4m/web/mvc/interceptors/LoginFormInterceptor.java
@@ -0,0 +1,64 @@
+package com.globacomp.ssystem.c4m.web.mvc.interceptors;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+public class LoginFormInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("In LoginInterceptor: In PreHandle:"+request.getRequestURI());
+		Map<String, Object> inputMap= (Map<String, Object>) RequestContextUtils.getInputFlashMap(request);
+		Iterator iterator = null;
+		
+		if(inputMap!= null && inputMap.size()>0) {
+			
+			iterator = inputMap.keySet().iterator();
+			
+			String key;
+			
+			while (iterator.hasNext()) {
+				key = (String) iterator.next();
+				System.out.println("key:"+key+" ==  Value:"+inputMap.get(key));
+			}
+		}
+		else
+			System.out.println("Map Empty");
+		return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("In LoginInterceptor: Post Handle: "+request.getRequestURI());
+		Map<String, Object> inputMap= (Map<String, Object>) RequestContextUtils.getInputFlashMap(request);
+		Iterator iterator = null;
+		
+		if(inputMap != null && inputMap.size()>0) {
+			
+			iterator = inputMap.keySet().iterator();
+			
+			String key;
+			
+			while (iterator.hasNext()) {
+				key = (String) iterator.next();
+				System.out.println("key:"+key+" ==  Value:"+inputMap.get(key));
+			}
+		}
+		else
+			System.out.println("Map Empty");
+		
+		super.postHandle(request, response, handler, modelAndView);
+	}
+}
